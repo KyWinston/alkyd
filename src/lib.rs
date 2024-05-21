73016c4a36017cebb8647d175136abcfb2fe5c34
KyleWinston <kyle.w.r@hotmail.com>
2024-05-21 12:05:53 -0400

    embedded fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-use bevy::prelude::*;
+use bevy::{asset::embedded_asset, prelude::*};
 use bevy_inspector_egui::quick::ResourceInspectorPlugin;
 use compute::plugin::{AppComputePlugin, AppComputeWorkerPlugin};
-use embedded::EmbeddedAssetPlugin;
 use materials::{
     painterly::PainterlyMaterial, resources::MaterialsInspector, MaterialSwatchPlugin,
 };
@@ -17,7 +16,6 @@ pub struct AlkydPlugin {
 pub struct Debug(pub bool);
 
 pub mod compute;
-pub mod embedded;
 pub mod materials;
 pub mod utilities;
 
@@ -25,15 +23,17 @@ impl Plugin for AlkydPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((
             MaterialSwatchPlugin { debug: self.debug },
-            EmbeddedAssetPlugin,
             MaterialPlugin::<PainterlyMaterial>::default(),
             UtilitiesPlugin,
             AppComputePlugin,
             AppComputeWorkerPlugin::<VoronoiWorker>::default(),
         ));
+        embedded_asset!(app, "src", "utilities/noise.wgsl");
+        embedded_asset!(app, "src", "materials/painterly_material.wgsl");
         app.add_event::<LoadNoise>()
             .insert_resource::<VoronoiImage>(VoronoiImage(None))
             .insert_resource::<Debug>(Debug(self.debug));
+        
         if self.debug {
             app.add_plugins(ResourceInspectorPlugin::<MaterialsInspector>::default());
             app.init_resource::<MaterialsInspector>()
