e00e5c128b2d900c8a22d92747b88e62d94ac7a7
KyleWinston <kyle.w.r@hotmail.com>
2025-01-30 17:58:27 -0500

    grass shader 2nd run

--- .github/workflows/scripts/run_coverage.sh
+++ .github/workflows/scripts/run_coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeu
 
-RUSTFLAGS="-Zinstrument-coverage" cargo test --lib
+RUSTFLAGS="-Z instrument-coverage" cargo test --lib
 grcov . --binary-path ./target/debug/ -s . -t html --branch --ignore-not-existing -o ./coverage/ --excl-start "#\[cfg\(test\)\]" --keep-only **/src/**/*
