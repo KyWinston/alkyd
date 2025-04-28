3f00a575c342939ed4269f7e3ee6c5e9b83720b4
KyleWinston <kyle.w.r@hotmail.com>
2025-04-28 06:00:59 -0400

    revert wesl

--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -14,68 +14,68 @@ impl Plugin for UtilitiesPlugin {
             app,
             GLOBAL_VALUES_HANDLE,
             "../../assets/shader_utils/consts/globs.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
 
         load_internal_asset!(
             app,
             NOISE_COMPUTE_HANDLE,
             "../../assets/shader_utils/noise/noise_compute.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
         load_internal_asset!(
             app,
             TEX_GEN_HANDLE,
             "../../assets/shader_utils/noise/tex_gen.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
         load_internal_asset!(
             app,
             SPRITELY_HANDLE,
             "../../assets/shader_utils/sprite_rotation.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
         load_internal_asset!(
             app,
             NOISE_FUNCTIONS_HANDLE,
             "../../assets/shader_utils/general.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
         load_internal_asset!(
             app,
             SIMPLEX_HANDLE,
             "../../assets/shader_utils/noise/simplex_3d.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
         load_internal_asset!(
             app,
             SIMPLEX_4D_HANDLE,
             "../../assets/shader_utils/noise/simplex_4d.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
         load_internal_asset!(
             app,
             NOISE_GEN_UTILS_HANDLE,
             "../../assets/shader_utils/noise/noise_gen.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
         load_internal_asset!(
             app,
             SOBEL_HANDLE,
             "../../assets/shader_utils/filters/sobel.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
         load_internal_asset!(
             app,
             BLEND_MODES_HANDLE,
             "../../assets/shader_utils/color_functions/blend_modes.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
         load_internal_asset!(
             app,
             CONVERTERS_HANDLE,
             "../../assets/shader_utils/color_functions/convert.wgsl",
-            Shader::from_wesl
+            Shader::from_wgsl
         );
     }
 }
